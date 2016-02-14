b214ac9e70832ab228818843ad242ed657f1a832
Sadayuki Kato <sadayuki.kato@gmail.com>
2016-02-14 19:15:37 +0900

    Upgrade to swagger-ui 2.1.4.

--- app/assets/javascripts/swagger-ui/index.js
+++ app/assets/javascripts/swagger-ui/index.js
@@ -1,13 +1,12 @@
-//= require ./lib/shred.bundle.js
 //= require ./lib/jquery-1.8.0.min.js
 //= require ./lib/jquery.slideto.min.js
 //= require ./lib/jquery.wiggle.min.js
 //= require ./lib/jquery.ba-bbq.min.js
-//= require ./lib/handlebars-1.0.0.js
+//= require ./lib/handlebars-2.0.0.js
 //= require ./lib/underscore-min.js
 //= require ./lib/backbone-min.js
-//= require ./lib/swagger.js
-//= require ./lib/swagger-client.js
 //= require ./swagger-ui.js
 //= require ./lib/highlight.7.3.pack.js
+//= require ./lib/jsoneditor.min.js
+//= require ./lib/marked.js
 
